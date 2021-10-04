b6bc060551fc297cb8f6c1f922e5e7a77a93f838
mhmtkrl <koral.mehmet45@gmail.com>
2021-10-04 23:37:32 +0300

    ready to develop gpio driver

--- Embedded Bootloader Project/main.c
+++ Embedded Bootloader Project/main.c
@@ -1,13 +1,6 @@
 #include "flash_config.h"
 #include "program_memory.h"
 
-int16_t i = 0;
-uint32_t adr;
-
 int main() {
-	adr = (uint32_t)APPLICATION_FIRMWARE_BASE_ADDRESS;
-	program_Memory_Page_Erase(APPLICATION_FIRMWARE_BASE_ADDRESS);
-	for(i = 0 ; i <= 128 ; i+=4) {
-		program_Memory_Fast_Word_Write((APPLICATION_FIRMWARE_BASE_ADDRESS + i), i);
-	}
+	
 }
