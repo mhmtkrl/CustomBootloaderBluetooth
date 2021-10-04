1205c298217731bba66af5d117a80927fdf907f1
mhmtkrl <koral.mehmet45@gmail.com>
2021-10-04 22:35:52 +0300

    erase page16 and write half page (miss 1 byte)

--- Embedded Bootloader Project/Objects/main.d
+++ Embedded Bootloader Project/Objects/main.d
@@ -1,3 +1,4 @@
 .\objects\main.o: main.c
 .\objects\main.o: flash_config.h
 .\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: program_memory.h
--- Embedded Bootloader Project/main.c
+++ Embedded Bootloader Project/main.c
@@ -1,8 +1,11 @@
 #include "flash_config.h"
+#include "program_memory.h"
 
-uint8_t optLock;
+int16_t i = 0;
 
 int main() {
-	program_Memory_Page_Erase(0x08001000);
-	program_Memory_Fast_Word_Write(0x08001000, 0xAABBCCDD);
+	program_Memory_Page_Erase(page16);
+	for(i = 0 ; i < 128 ; i+=4) {
+		program_Memory_Fast_Word_Write((page16 + i), i);
+	}
 }
