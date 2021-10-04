3e3e818d80ad95aa02e85483ab4d6b5012fb6419
mhmtkrl <koral.mehmet45@gmail.com>
2021-10-04 23:06:01 +0300

    define bootlaoder size

--- Embedded Bootloader Project/main.c
+++ Embedded Bootloader Project/main.c
@@ -2,10 +2,12 @@
 #include "program_memory.h"
 
 int16_t i = 0;
+uint32_t adr;
 
 int main() {
-	program_Memory_Page_Erase(page16);
+	adr = (uint32_t)APPLICATION_FIRMWARE_BASE_ADDRESS;
+	program_Memory_Page_Erase(APPLICATION_FIRMWARE_BASE_ADDRESS);
 	for(i = 0 ; i <= 128 ; i+=4) {
-		program_Memory_Fast_Word_Write((page16 + i), i);
+		program_Memory_Fast_Word_Write((APPLICATION_FIRMWARE_BASE_ADDRESS + i), i);
 	}
 }
--- Embedded Bootloader Project/program_memory.h
+++ Embedded Bootloader Project/program_memory.h
@@ -1,1 +1,8 @@
-#define page16	0x08001000
+//Table 11 NVM module organization (Cat.5 devices) page 56 RM0038
+#define PAGE_SIZE										  256													//256 bytes
+#define PROGRAM_MEMORY_BASE_ADDRESS		0x08000000
+
+//Bootloader size is 32 kB
+//Each sector is 4 kB, so first 7 Sectors are used for the bootloader 
+#define BOOTLOADER_SIZE_KB		32
+#define APPLICATION_FIRMWARE_BASE_ADDRESS		((PROGRAM_MEMORY_BASE_ADDRESS) + ((BOOTLOADER_SIZE_KB) * 1024))
