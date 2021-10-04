16cb68c6d3cd5b99f8e0fd25b8e96b0ec5ab6d36
mhmtkrl <koral.mehmet45@gmail.com>
2021-10-04 22:36:59 +0300

    write half page is ok now

--- Embedded Bootloader Project/main.c
+++ Embedded Bootloader Project/main.c
@@ -5,7 +5,7 @@ int16_t i = 0;
 
 int main() {
 	program_Memory_Page_Erase(page16);
-	for(i = 0 ; i < 128 ; i+=4) {
+	for(i = 0 ; i <= 128 ; i+=4) {
 		program_Memory_Fast_Word_Write((page16 + i), i);
 	}
 }
